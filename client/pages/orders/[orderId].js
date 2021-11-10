05aad98c571a05b62b692b4b2517f3d701d2a696
Omar <omar@kamelizer.com>
2021-11-11 00:47:38 +0200

    add payment for a ticket order

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,8 +1,19 @@
 import { useEffect, useState } from "react";
+import StripCheckout from "react-stripe-checkout";
+import useRequest from "../../hooks/use-request";
 
-const OrderShow = ({order}) => {
+const OrderShow = ({order, currentUser}) => {
     
-    const [timeLeft, setTimeLeft] = useState('');
+    const [timeLeft, setTimeLeft] = useState(0);
+
+    const {doRequest, errors} = useRequest({
+        url:'/api/payments',
+        method:'post',
+        body:{
+            orderId:order.id
+        },
+        onSuccess: (payment) => consolt.log(payment)
+    })
 
     useEffect(() => {
         const findTimeLeft = () => {
@@ -10,11 +21,28 @@ const OrderShow = ({order}) => {
             setTimeLeft(Math.round(msLeft / 1000))
         }
 
-        setInterval(findTimeLeft, 1000);
+        const intervalId = setInterval(findTimeLeft, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        }
     }, [])
 
+    if(timeLeft < 0 ){
+        return <div>Order expired</div>
+    }
 
-    return <div>{msLeft/1000} seconds untill order expires</div>
+
+    return <div>
+            {msLeft/1000} seconds untill order expires
+            <StripCheckout 
+                token={(token) => console.log(token)}
+                stripeKey=""
+                amount={order.ticket.price * 100}
+                email={currentUser.email}
+            />
+            {errors}
+        </div>
 
     return(
         <div>
