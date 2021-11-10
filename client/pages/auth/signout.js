c0aa9c6a5920e678ff7912123e8434734b38f43d
Omar <omar@kamelizer.com>
2021-11-10 10:06:35 +0200

    fix components export

--- client/pages/auth/signout.js
+++ client/pages/auth/signout.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
-export default () => {
+const SignOut = () => {
   const { doRequest } = useRequest({
     url: '/api/users/signout',
     method: 'post',
@@ -16,3 +16,5 @@ export default () => {
 
   return <div>Signing you out...</div>;
 };
+
+export default SignOut;
